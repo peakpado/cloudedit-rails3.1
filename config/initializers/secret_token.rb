78b1b4adf747adb9ee767f13b42358c000eb4a65
Peak Pado <peakpado@gmail.com>
2011-10-15 11:01:23 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudeditRails31::Application.config.secret_token = 'd5320367396631e2b015dd3af3dbc7c54027ad3bfff76bcc4fc5272b3c5c1d6f0d6096be2b047715f2f12c5bd82904070da2628fbcc1a4b76486aa22824f56a2'
